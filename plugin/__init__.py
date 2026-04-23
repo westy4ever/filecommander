9cce56cf3823d9aedae5a497ab19ad3fa8e06acd
westy4ever <drwessamelguindy@gmail.com>
2026-04-23 13:09:03 +0200

    Add files via upload

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,3 +1,4 @@
+--- plugin/__init__.py (原始)
 # -*- coding: utf-8 -*-
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
@@ -6,24 +7,56 @@ import gettext
 
 
 def localeInit():
-	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
-	os_environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-	gettext.bindtextdomain("FileCommander", resolveFilename(SCOPE_PLUGINS, "Extensions/FileCommander/locale"))
+        lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
+        os_environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
+        gettext.bindtextdomain("FileCommander", resolveFilename(SCOPE_PLUGINS, "Extensions/FileCommander/locale"))
 
 
 def _(txt):
-	t = gettext.dgettext("FileCommander", txt)
-	if t == txt:
-#		print "[FileCommander] fallback to default translation for", txt
-		t = gettext.gettext(txt)
-	return t
+        t = gettext.dgettext("FileCommander", txt)
+        if t == txt:
+#               print "[FileCommander] fallback to default translation for", txt
+                t = gettext.gettext(txt)
+        return t
 
 
 def ngettext(singular, plural, n):
-	t = gettext.dngettext('FileCommander', singular, plural, n)
-	if t in (singular, plural):
-		t = gettext.ngettext(singular, plural, n)
-	return t
+        t = gettext.dngettext('FileCommander', singular, plural, n)
+        if t in (singular, plural):
+                t = gettext.ngettext(singular, plural, n)
+        return t
+
+
+localeInit()
+language.addCallback(localeInit)
+
++++ plugin/__init__.py (修改后)
+# -*- coding: utf-8 -*-
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
+import os
+import gettext
+
+
+def localeInit():
+        lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
+        os.environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
+        gettext.bindtextdomain("FileCommander", resolveFilename(SCOPE_PLUGINS, "Extensions/FileCommander/locale"))
+
+
+def _(txt):
+        t = gettext.dgettext("FileCommander", txt)
+        if t == txt:
+#               print "[FileCommander] fallback to default translation for", txt
+                t = gettext.gettext(txt)
+        return t
+
+
+def ngettext(singular, plural, n):
+        t = gettext.dngettext('FileCommander', singular, plural, n)
+        if t in (singular, plural):
+                t = gettext.ngettext(singular, plural, n)
+        return t
 
 
 localeInit()
